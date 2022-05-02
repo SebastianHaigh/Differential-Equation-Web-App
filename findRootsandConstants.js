95f4efaf192b3bf3150625a5e6859e2b51dbee44
SebastianHaigh <sebhaigh27@googlemail.com>
2022-05-02 14:45:13 +0100

    Update to use new classes from structures.js

--- findRootsandConstants.js
+++ findRootsandConstants.js
@@ -1,45 +1,80 @@
-//class AnalyticSolution {
-//	constructor() {
-//
-//	}
-//	print();
-//}
-
-
-solution = function(dO) {
-   dO = makeGrid(dO);
-   dO = findRoots(dO);
-	if (dO.type == 1 || dO.type == 4) {
-		var i;
-		for (i = 0; i < dO.timeSeries.length; i++) {
+/**
+ * TODO: This funciton needs to be removed and its implementation spread among 
+ * the analytic solution classes.
+ */
+function solution(dO, equation_factory, solution_factory) {
+	//dO.differential_equation = equation_factory.create_differential_equation(1,2,3);
+   	dO = makeGrid(dO);
+   	dO = findRoots(dO);
+   	if (dO.type == 1 || dO.type == 4) {
+		for (let i = 0; i < dO.timeSeries.length; i++) {
 			dO.timeSeries[i] = distinctRealRoots(dO.root1.Re, dO.root2.Re, dO.solConstants, dO.timeSeries[i].t);
 		}
 	}
 
 	if (dO.type == 2) {
-		var i;
-		for (i = 0; i < dO.timeSeries.length; i++) {
+		for (let i = 0; i < dO.timeSeries.length; i++) {
 			dO.timeSeries[i] = complexRoots(dO.root1, dO.solConstants, dO.timeSeries[i].t);
 		}
 	}
 
 	if (dO.type == 3) {
-		var i;
-		for (i = 0; i < dO.timeSeries.length; i++) {
+		for (let i = 0; i < dO.timeSeries.length; i++) {
 			dO.timeSeries[i] = repeatedRoots(dO.root1.Re, dO.solConstants, dO.timeSeries[i].t);
 		}
 	}
-	
-	if (dO.type == 5) {
-		var i 
-		for (i = 0; i < dO.timeSeries.length; i++) {
-			dO.timeSeries[i] = 0;
-		}
+
+if (dO.type == 5) {
+	for (let i = 0; i < dO.timeSeries.length; i++) {
+		dO.timeSeries[i] = 0;
 	}
+}
 
 	return dO;
 };
 
+/**
+ * TODO: Move this implementation spread to the analytic solution classes.
+ */
+makeGrid = function(dO) {
+	var i;
+	var dt = 0.1;
+	for (i = 0; i < 1000; i++) {
+		dO.timeSeries[i]  = {t: dt * i, x: 0};
+		dO.stepResponse[i] = {t: dt * i, x: 0};
+	}
+	return dO;
+};
+
+/**
+ * TODO: Move this implementation spread to the analytic solution classes.
+ */
+distinctRealRoots = function(root1, root2, constants, point) {
+	outputPoint = constants.A*Math.exp(root1*point) + constants.B*Math.exp(root2*point);
+	return {t: point, x: outputPoint};
+};
+
+/**
+ * TODO: Move this implementation spread to the analytic solution classes.
+ */
+complexRoots = function(root, constants, point) {
+	var ex = Math.exp(root.Re*point);
+	var cos = Math.cos(root.Im*point);
+	var sin = Math.sin(root.Im*point);
+	outputPoint = ex*(constants.A*cos + constants.B*sin);
+	return {t: point, x: outputPoint};
+};
+
+/**
+ * TODO: Move this implementation spread to the analytic solution classes.
+ */
+repeatedRoots = function(root, constants, point) {
+	outputPoint = Math.exp(root*point)*(constants.A*point + constants.B);
+	return {t: point, x: outputPoint};
+};
+
+// TODO: No longer needed remove. However, pay attention that the 
+// updateRootDisplay and updateConstantDisplay
 function findRoots(dO) {
 	var a = dO.diffEqconts.a;
 	var b = dO.diffEqconts.b;
@@ -80,35 +115,24 @@ function findRoots(dO) {
 	return dO;
 };
 
+
+// TODO: No longer needed remove
 function updateConstantDisplay(A, B) {
 	$("#constant1").text(A.toFixed(2));
 	$("#constant2").text(B.toFixed(2));
 };
 
-function updateRootDisplay(root1, root2) {
-	$("#sigma1").text(root1.Re.toFixed(2));
-	$("#sigma2").text(root2.Re.toFixed(2));
-	$("#omega1").text(Math.abs(root1.Im).toFixed(2));
-	$("#omega2").text(Math.abs(root2.Im).toFixed(2));
-	if (root1.Im < 0) {
-		$("#sign1").text("-");
-	} else {
-		$("#sign1").text("+");
-	}
-  
-	if (root2.Im < 0) {
-		$("#sign2").text("-");
-	} else {
-		$("#sign2").text("+");
-	}
-};
+// TODO: Should be made to funciton with the new root class. 
 
+
+// TODO: This can be removed, its functionality has been moved to a class
 function getRootsDistinct(a, b, c) {
 	var sigma1 = (-b + Math.sqrt((b*b) - (4*a*c)))/(2*a);
 	var sigma2 = (-b - Math.sqrt((b*b) - (4*a*c)))/(2*a);
 	return {root1: {Re: sigma1, Im: 0}, root2: {Re: sigma2, Im: 0}};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 function getRootsComplex(a, b, c) {
 	var sigma = -b/(2*a);
 	var omega1 = Math.sqrt(Math.abs((b*b) - (4*a*c)))/(2*a);
@@ -116,21 +140,25 @@ function getRootsComplex(a, b, c) {
 	return {root1: {Re: sigma, Im: omega1}, root2: {Re: sigma, Im: omega2}};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 function getRootsRepeated(a, b, c) {
 	var sigma = -b/(2*a);
 	return {root1: {Re: sigma, Im: 0}, root2: {Re: sigma, Im: 0}};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 function getRootsFirstOrder(a, b, c) {
 	var sigma = -c/b;
 	return {root1: {Re: sigma, Im: 0}, root2: {Re: 0, Im: 0}};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 function getRootsZerothOrder(a, b, c) {
 	var sigma = -c;
 	return {root1: {Re: sigma, Im: 0}, root2: {Re: 0, Im: 0}};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 function determineRootType(a, b, c) {
 	var bSquared = Math.pow(b, 2);
 	var ac4 = 4*a*c;
@@ -158,6 +186,7 @@ function determineRootType(a, b, c) {
 	return type;
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 distinctRootsConstants = function(root1, root2, x, xdash) {
 	var A, B;
 	A = (xdash - (root2.Re*x))/(root1.Re - root2.Re);
@@ -165,6 +194,7 @@ distinctRootsConstants = function(root1, root2, x, xdash) {
 	return {A: A, B: B};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 complexRootsConstants = function(root, x, xdash) {
 	var A, B;
 	A = x;
@@ -172,6 +202,7 @@ complexRootsConstants = function(root, x, xdash) {
 	return {A: A, B: B};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 repeatedRootsConstants = function(root, x, xdash) {
 	var A, B;
 	A = x;
@@ -179,6 +210,7 @@ repeatedRootsConstants = function(root, x, xdash) {
 	return {A: A, B: B};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 firstOrderConstants = function(root, x, xdash) {
 	var A, B;
 	A = xdash/root.Re;
@@ -186,34 +218,10 @@ firstOrderConstants = function(root, x, xdash) {
 	return {A: A, B: B};
 };
 
+// TODO: This can be removed, its functionality has been moved to a class
 zerothOrderConstants = function() {
 	return {A: 0, B: 0};
 };
 
-makeGrid = function(dO) {
-	var i;
-	var dt = 0.1;
-	for (i = 0; i < 1000; i++) {
-		dO.timeSeries[i]  = {t: dt * i, x: 0};
-		dO.stepResponse[i] = {t: dt * i, x: 0};
-	}
-	return dO;
-};
 
-distinctRealRoots = function(root1, root2, constants, point) {
-	outputPoint = constants.A*Math.exp(root1*point) + constants.B*Math.exp(root2*point);
-	return {t: point, x: outputPoint};
-};
 
-complexRoots = function(root, constants, point) {
-	var ex = Math.exp(root.Re*point);
-	var cos = Math.cos(root.Im*point);
-	var sin = Math.sin(root.Im*point);
-	outputPoint = ex*(constants.A*cos + constants.B*sin);
-	return {t: point, x: outputPoint};
-};
-
-repeatedRoots = function(root, constants, point) {
-	outputPoint = Math.exp(root*point)*(constants.A*point + constants.B);
-	return {t: point, x: outputPoint};
-};
--- sliderControl.js
+++ sliderControl.js
@@ -11,7 +11,9 @@ var bOutput = document.getElementById("bOutput");
 var cOutput = document.getElementById("cOutput");
 var ic1Output = document.getElementById("ic1Output");
 var ic2Output = document.getElementById("ic2Output");
+var bobby = document.getElementById("oldbob");
 //var resetButton = document.getElementById("resetButton");
+
 
 // I do not know what this is doing, I will have to find out by experimenting 
 // and then document the purpose.
@@ -21,21 +23,17 @@ cOutput.innerHTML = cSlider.value;
 ic1Output.innerHTML = ic1Slider.value;
 ic2Output.innerHTML = ic2Slider.value;
 
+MathJax.Hub.Config({processSectionDelay: 0, processUpdateTime: 0, processUpdateDelay: 0});
+
 // Update the current slider value (each time you drag the slider handle)
 
 // I am not convinced that the data object should be created or defined in this file.
 var data_object = initDataObject(0, 0, 0, 0, 0);
 
-data_object = solution(data_object);
-
 // Initialisation of plots and the ball simulation (should this be in a file 
 // called sliderControl?)
 
-InitChart(data_object.timeSeries);
 
-poleplot([data_object.root1, data_object.root2]);
-
-initBall(data_object.intCon.x);
 
 // State variable, what are all the possibel values that this can take?
 var state = 0;
@@ -56,31 +54,37 @@ startButton.onclick = function() {
 		$("#interval3").text(refreshIntervalId);
 	};
 	d3.selectAll("svg > *").remove();
-	data_object = solution(data_object);
+	//data_object = solution(data_object);
+	data_object = solve_equation(data_object);
 	InitChart(data_object.timeSeries);
-	poleplot([data_object.root1, data_object.root2]);
+	var roots = data_object.differential_equation.get_roots();
+	var roots_for_pole_plot = [];
+	for (let i = 0; i < roots.length; i++) {
+		roots_for_pole_plot[i] = {Re: roots[i].re(), Im: roots[i].im()};
+	}
+	poleplot(roots_for_pole_plot);
 	refreshIntervalId = ball(data_object.timeSeries);
 	$("#interval3").text(refreshIntervalId);
 	state = 1;
 };
 
-
 aSlider.oninput = function() {
-	data_object.diffEqconts.a = (this.value)/10;
-    $("#aOutput").text(data_object.diffEqconts.a);
+	data_object.constants.a = (this.value)/100;
+    $("#aOutput").text(data_object.constants.a);
 	data_object = updateAll(data_object);
 };
 
 bSlider.oninput = function() {
-	data_object.diffEqconts.b = (this.value)/10;
-    $("#bOutput").text(data_object.diffEqconts.b);
+	data_object.constants.b = (this.value)/100;
+    $("#bOutput").text(data_object.constants.b);
 	data_object = updateAll(data_object);
 };
 cSlider.oninput = function() {
-	data_object.diffEqconts.c = (this.value)/10;
-    $("#cOutput").text(data_object.diffEqconts.c);
+	data_object.constants.c = (this.value)/100;
+    $("#cOutput").text(data_object.constants.c);
 	data_object = updateAll(data_object);
 };
+
 ic1Slider.oninput = function() {
 	data_object.intCon.x = (this.value)/10;
     $("#ic1Output").text(data_object.intCon.x);
@@ -93,88 +97,74 @@ ic2Slider.oninput = function() {
 	data_object = updateAll(data_object);
 };
 
-// Data object should be a class 
-
-class DataObject {
-	constructor() {}
-};
-
-
-class TimeSeries {
-	constructor() {
-		this.series = [];
-	}
-
-	push(time, data) { this.series.push({time: time, data: data}); }
-
-	at(i) { 
-		if (i < this.length()) {
-			return this.series[i]; 
-		} else {
-			return {time: -1, data: -1};
-		}
-	}
-
-	length() { return this.series.length; }
-};
-
+/**
+ * Initialises the Global data object.
+ *  
+ * This object stores all of the relevant information needed for the display.
+ * */ 
 function initDataObject(a, b, c, u, v) {
 	var dataObject = {
 		type: 0,
-		root1: {Re: 0, Im: 0}, 
-		root2: {Re: 0, Im: 0}, 
-		diffEqconts: {a: a, b: b, c: c},
+		roots: [{Re: 0, Im: 0}],
+		constants: {a: a, b: b, c: c},
 		intCon: {t: 0, x: u, xdash: v},
-		solConstants: {A: 0, B: 0}, 
 		timeSeries: [{t: 0, x: 0}],
 		stepResponse: [{t: 0, x: 0}],
-		ts: new TimeSeries()};
+		differential_equation: 0,
+		solution: 0};
 	return dataObject;
+};
+
+function solve_equation(data_object) {
+	let equation_factory = new DifferentialEquationFactory();
+	let solution_factory = new SolutionFactory();
+	data_object.differential_equation = equation_factory.create_differential_equation(data_object.constants.a, data_object.constants.b, data_object.constants.c);
+	data_object.solution = solution_factory.new_solution(data_object.differential_equation, data_object.intCon.x, data_object.intCon.xdash);
+	data_object.timeSeries = data_object.solution.time_series(0.1, 1000);
+
+	return data_object;
 };
 
 function updateAll(data_object) {
 	d3.selectAll("svg > *").remove();
-	data_object = solution(data_object);
-	InitChart(data_object.timeSeries);
+	data_object = solve_equation(data_object);
+	InitChart(data_object.solution.time_series(0.1, 1000));
 	initBall(data_object.intCon.x);
-	poleplot([data_object.root1, data_object.root2]);
+	var roots = data_object.differential_equation.get_roots();
+	var roots_for_pole_plot = [];
+	for (let i = 0; i < roots.length; i++) {
+		roots_for_pole_plot[i] = {Re: roots[i].re(), Im: roots[i].im()};
+	}
+	poleplot(roots_for_pole_plot);
+	updateRootDisplay(roots[0], roots[1]);
 	return data_object;
 };
 
+function updateRootDisplay(root1, root2) {
+	$("#sigma1").text(root1.print_real());
+	$("#sigma2").text(root2.print_real());
+
+	$("#omega1").text(Math.abs(root1.im()).toFixed(2));
+	$("#omega2").text(Math.abs(root2.im()).toFixed(2));
+	if (root1.im() < 0) {
+		$("#sign1").text("-");
+	} else {
+		$("#sign1").text("+");
+	}
+  
+	if (root2.im() < 0) {
+		$("#sign2").text("-");
+	} else {
+		$("#sign2").text("+");
+	}
+};
+
+// Interval for updating MathJax
 setInterval(function() {
-	var solOut;
-	var sign = " ";
-	var signb = " ";
-	var signc = " ";
-	if(data_object.solConstants.B<0) {
-		sign = " ";
-	} else {
-		sign = "+";
-	}
-	if (data_object.diffEqconts.b < 0) {
-		signb = " ";
-	} else {
-		signb = "+";
-	}
-	if (data_object.diffEqconts.c < 0) {
-		signc = " ";
-	} else {
-		signc = "+";
-	}
-		
-	
-var mathOut = " " + data_object.diffEqconts.a + "\\frac{d^2x}{dt^2}" +signb+ " " + data_object.diffEqconts.b + "\\frac{dx}{dt}" + signc + " " + data_object.diffEqconts.c + "x = 0, \\: x(0) = " + data_object.intCon.x + ", x'(0) = " + data_object.intCon.xdash ;
-
-if (data_object.type == 1) {
-	solOut =  "x(t) = " + data_object.solConstants.A.toFixed(2) + "e^{" + data_object.root1.Re.toFixed(2) + "t}" +sign+ "" + data_object.solConstants.B.toFixed(2) + "e^{" + data_object.root2.Re.toFixed(2) + "t}";
-};
-
-if (data_object.type == 2) {
-solOut =  "x(t) = e^{" + data_object.root1.Re.toFixed(2) + "t}(" + data_object.solConstants.A.toFixed(2) + "\\cos(" + data_object.root1.Im.toFixed(2) +"t)" +sign+ "" + data_object.solConstants.B.toFixed(2) + "\\sin(" + Math.abs(data_object.root2.Im.toFixed(2)) +"t))";
-};
 
 	var math = MathJax.Hub.getAllJax("diffEq")[0];
-	MathJax.Hub.Queue(["Text",math,mathOut]);
+	MathJax.Hub.Queue(["Text",math,data_object.differential_equation.print()]);
 	var math2 = MathJax.Hub.getAllJax("solEq")[0];
-	MathJax.Hub.Queue(["Text",math2,solOut]);
-}, 600)
+	MathJax.Hub.Queue(["Text",math2,data_object.solution.print()]);
+
+}, 500)
